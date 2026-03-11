ae88d260718e385736b2625a5065d1ee52e927c2
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2026-03-11 08:50:31 -0300

    Aula de interface

---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,34 @@
+﻿IAnimal animal1 = new Cachorro();
+
+IAnimal animal2 = new Gato();
+
+animal1.FazerSom();
+animal2.FazerSom();
+
+Cachorro animal3 = new Cachorro();
+animal3.QuantidadeDePatas();
+
+public interface IAnimal
+{
+    public void FazerSom();
+}
+
+public class Cachorro : IAnimal
+{
+    public void FazerSom()
+    {
+        Console.WriteLine("AuAuauauau!");
+    }
+    public void QuantidadeDePatas()
+    {
+        Console.WriteLine("Tem 4 patas");
+    }
+}
+public class Gato : IAnimal
+{
+    public void FazerSom()
+    {
+        Console.WriteLine("Miau!");
+    }
+}
+
