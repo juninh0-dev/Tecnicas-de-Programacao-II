7e16e0e8af8f9acf697f01fcb4f91f277eaff8df
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2026-03-11 08:49:57 -0300

    Aula de herança

---
+++ aula0223/Heranca/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+Aluno aluno1 = new Aluno("Paulo", "111.111.111-11", "0201392511050");
+Console.WriteLine($"Nome:{aluno1.Nome}\n CPF:{aluno1.Cpf}\n" +
+    $"RA:{aluno1.Ra}");
+
+Console.WriteLine("Digite o nome do professor");
+var nomeProf = Console.ReadLine();
+
+Console.WriteLine("Digite o cpf do professor");
+var cpfProf = Console.ReadLine();
+
+Console.WriteLine("Digite o titulação do professor");
+var titutalacaoProf = Console.ReadLine();
+
+Professor prof1 = new Professor(nomeProf, cpfProf, titutalacaoProf); 
+Professor.Exibir(prof1);
+
+Console.ReadKey();
+
+public class Pessoa {
+    public Pessoa(string? nome, string? cpf) {
+        Nome = nome;
+        Cpf = cpf;
+    }
+
+    public string? Nome { get; set; }
+    public string? Cpf { get; set; }
+}
+
+
+
+public class Aluno : Pessoa
+{
+    public Aluno(string? nome, string? cpf, string ra):base(nome,cpf)
+    {
+        Ra = ra;
+    }
+    public string? Ra { get; set; }
+}
+
+public class Professor : Pessoa
+{
+    public Professor(string? nome, string? cpf, string titulacao) : base(nome, cpf)
+    {
+        Titulacao = titulacao;
+    }
+
+    public static void Exibir(Professor prof) => Console.WriteLine($"{prof.Nome}\n{prof.Cpf}\n{prof.Titulacao}");
+
+    public string? Titulacao { get; set; }
+   
+}
+
---
+++ aula0223/aula0223/Program.cs
@@ -0,0 +1,36 @@
+﻿//usando
+using System.Reflection;
+using OrientacaoObejto;
+
+Produto prod1 = new Produto();
+prod1.Nome = "Caderno";
+prod1.Preco = 12.90;
+//objeto prod1
+Console.WriteLine("Nome: " + prod1.Nome);
+Console.WriteLine($"Preço: {prod1.Preco}");
+
+
+Produto prod2 = new Produto("Láps", 1.50);
+//objeto prod2
+Console.WriteLine("Nome: " + prod2.Nome);
+Console.WriteLine($"Preço: {prod2.Preco}");
+
+//objeto de Produto2
+
+Produto2 prod3 = new("Iphone 18 PRO max", 99.90, 500); // Pode ser criado um novo objeto sem o Produto3();
+prod3.Exibir();
+
+Console.ReadKey();
+//declarando
+
+public class Produto
+{
+    public Produto(){}
+    public Produto(string? nome, double Preco)
+    {
+        Nome = nome;
+        this.Preco = Preco;
+    }
+    public string? Nome { get; set; }
+    public double Preco { get; set; }
+}
