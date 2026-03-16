8f69cdacaa069af9f611c12e080647c5296168e7
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2026-03-16 10:02:30 -0300

    Adicionando arquivos Sincrono_Assincrono

---
+++ Sincrono_Assincrono/Assincrono/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+
+Console.WriteLine("Café da manhã sincrono");
+cafeDaManhaAsync();
+Console.WriteLine("Fim do Café da manhã");
+
+static async void cafeDaManhaAsync()
+{
+    Console.WriteLine("Preparar o café");
+    var TarefaCafe = PrepararCafeAsync();
+    Console.WriteLine("\n Preparar o pão");
+    var TarefaPao = PrepararPaoAsync();
+    var cafe = await (TarefaCafe);
+    var pao = await (TarefaPao);
+    ServirCafeAsync(cafe, pao);
+}
+
+static void ServirCafeAsync(Cafe cafe, Pao pao)
+{
+    Console.WriteLine("\n Servindo café da manhã");
+    Thread.Sleep(2000);
+    Console.WriteLine("\n Café da manhã servido.");
+}
+
+static async Task<Pao> PrepararPaoAsync()
+{
+    Console.WriteLine("\n Partir o pão");
+    await Task.Delay(2000);
+    Console.WriteLine("\n Passar manteiga");
+    await Task.Delay(2000);
+    return new Pao();
+}
+
+static async Task<Cafe> PrepararCafeAsync()
+{
+    Console.WriteLine("\n Ferver a água");
+    await Task.Delay(2000);
+    Console.WriteLine("\n Coar o café");
+    await Task.Delay(2000);
+    return new Cafe();
+}
+
+internal class Cafe
+{
+
+}
+internal class Pao
+{
+
+}
---
+++ Sincrono_Assincrono/Sincrono/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+
+Console.WriteLine("Café da manhã sincrono");
+cafeDaManha();
+Console.WriteLine("Fim do Café da manhã");
+
+static void cafeDaManha()
+{
+    Console.WriteLine("Preparar o café");
+    var cafe = PrepararCafe();
+    Console.WriteLine("\n Preparar o pão");
+    var pao = PrepararPao();
+    ServirCafe(cafe, pao);
+}
+
+static void ServirCafe(Cafe cafe, Pao pao)
+{
+    Console.WriteLine("\n Servindo café da manhã");
+    Thread.Sleep(2000);
+    Console.WriteLine("\n Café da manhã servido.");
+}
+
+static Pao PrepararPao()
+{
+    Console.WriteLine("\n Partir o pão");
+    Thread.Sleep(2000);
+    Console.WriteLine("\n Passar manteiga");
+    Thread.Sleep(2000);
+    return new Pao();
+}
+
+static Cafe PrepararCafe()
+{
+    Console.WriteLine("\n Ferver a água");
+    Thread.Sleep(2000);
+    Console.WriteLine("\n Coar o café");
+    Thread.Sleep(2000);
+    return new Cafe();
+}
+
+internal class Cafe
+{
+
+}
+internal class Pao
+{
+
+}
