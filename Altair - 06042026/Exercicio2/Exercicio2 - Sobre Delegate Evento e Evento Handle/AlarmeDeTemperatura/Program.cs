a3e3682df41ce9bb399d7cc512d268a0379e5e8a
Altair Preto de Godoi Junior <altair.jr2006@gmail.com>
2026-04-13 10:57:04 -0300

    teste

---
+++ Altair - 06042026/Exercicio2/Exercicio2 - Sobre Delegate Evento e Evento Handle/AlarmeDeTemperatura/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Data;
+ArCondicionado ar1 = new ArCondicionado();
+ar1.temperatura = 18;
+ar1.superior = 25;
+ar1.inferior = 10;
+
+// Inscrevendo corretamente
+ar1.AlarmeTemperaturaAlta += Monitor.tempSuperior;
+ar1.AlarmeTemperaturaBaixa += Monitor.tempInferior;
+
+ar1.temperaturaAjustada(30); // testa superior
+ar1.temperaturaAjustada(5);  // testa inferior
+
+ar1.alarmeDisparado();
+
+delegate void AlarmeTemperatura();
+
+class ArCondicionado()
+{
+    public double? temperatura { get; set; }
+    public int? superior { get; set; }
+    public int? inferior { get; set; }
+
+    public event AlarmeTemperatura? AlarmeTemperaturaAlta;
+    public event AlarmeTemperatura? AlarmeTemperaturaBaixa;
+    public void alarmeDisparado()
+    {
+        Console.WriteLine("Iniciando a verificação de temperatura");
+        if (temperatura > superior)
+        {
+            AlarmeTemperaturaAlta();
+        }
+        else if (temperatura < inferior)
+        {
+            AlarmeTemperaturaBaixa();
+        }
+    }
+    public void temperaturaAjustada(double? novaTemp)
+    {
+        temperatura = novaTemp;
+        alarmeDisparado();
+    }
+
+}
+
+class Monitor
+{
+    public static void tempSuperior()
+    {
+        Console.WriteLine("A temperatura passou do limite superior!");
+    }
+    public static void tempInferior()
+    {
+        Console.WriteLine("A temperatura passou do limite inferior!");
+    }
+}
