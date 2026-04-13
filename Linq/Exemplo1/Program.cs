cc65bb96e9e7f462b453699824bb98eb9a79edd5
Altair Preto de Godoi Junior <altair.jr2006@gmail.com>
2026-04-13 09:13:57 -0300

    Adicionando exemplos de LINQ com lambda

---
+++ Linq/Exemplo1/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] numeros = {1, 2, 3, 4, 5, 6};
+var pares = from n in numeros where n%2 == 0 select n;
+foreach (var num in pares)
+{
+    Console.WriteLine(num);
+}
+
+//outra sintaxe usando lambida
+Console.WriteLine("Exemplo Usando Lambda\n");
+var pares2 = numeros.Where(n => n % 2 == 0);
+foreach (var num in pares2)
+{
+    Console.WriteLine(num);
+}
---
+++ Linq/Exemplo3/Program.cs
@@ -0,0 +1,87 @@
+﻿var listaProdutos = Produto.GetProdutos();
+Console.WriteLine("Produtos Eleetrônicos");
+
+var produtosEletronicos = listaProdutos.Where(p => p.Categoria == "Eletrônicos");
+
+Produto.Mostrar(produtosEletronicos);
+
+Console.WriteLine("--------------------------");
+
+Console.WriteLine("Produtos Caros com estoque maior do que 5");
+
+var produtosCaros = listaProdutos.Where(p =>p.Preco >= 1000 && p.Estoque > 5);
+Produto.Mostrar(produtosCaros);
+
+Console.WriteLine("--------------------------");
+
+Console.WriteLine("Produtos ");
+
+var estoqueBaixo = listaProdutos.Where(p => p.Estoque < 15).OrderBy(p=> p.Nome);
+Produto.Mostrar(estoqueBaixo);
+
+Console.WriteLine("--------------------------");
+
+var produtosOrdenados = listaProdutos.OrderBy(p => p.Categoria).ThenBy(p => p.Nome);
+
+Console.WriteLine("--------------------------");
+
+Console.WriteLine("Filtrando preços menor do que 500 com aumento de 10% ordenado " +
+    "por nome e criando um tipo de anônimo.");
+
+var resultado = listaProdutos.Where(p => p.Preco < 500).OrderBy(p=>p.Nome)
+    .Select(p => new {nomeProduto = p.Nome.ToUpper(), PrecoComAumento = p.Preco *1.1});
+Produto.Mostrar(resultado);
+Console.WriteLine("--------------------------");
+
+Console.WriteLine("Valor médio dos preços dos eletrônicos.");
+
+double media = listaProdutos.Where(p => p.Categoria == "Eletrônicos").Average(p => p.Preco);
+
+Console.WriteLine($"Média = {media}");
+
+Console.WriteLine("Selecionar produtos com preços maior do que 200 com desconto de 20%  " +
+    "ordenado pelo preço criando um tipo anônimo");
+
+var resultado2 = listaProdutos.Where(p => p.Preco > 200).OrderBy(p=>p.Preco)
+    .Select(p => new { 
+        nomeProduto = p.Nome.ToUpper(), 
+        PrecoComDesconto = p.Preco * 0.8 
+    });
+Produto.Mostrar(resultado);
+Console.WriteLine("--------------------------");
+
+Console.ReadKey();
+
+public class Produto
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public double Preco { get; set; }
+    public int Estoque { get; set; }
+    public string? Categoria { get; set; }
+    public static List<Produto> GetProdutos()
+    {
+        List<Produto> produtos = new List<Produto>()
+        {
+            new Produto { Id = 1, Nome = "Camiseta", Preco = 29.99, Estoque = 50, Categoria = "Roupa" },
+            new Produto { Id = 2, Nome = "Meia", Preco = 20.99, Estoque = 100, Categoria = "Roupa" },
+            new Produto { Id = 3, Nome = "Tênis Nike", Preco = 400.50, Estoque = 10, Categoria = "Calçado" },
+            new Produto { Id = 4, Nome = "Celular", Preco = 5000, Estoque = 150, Categoria = "Eletrônicos" },
+            new Produto { Id = 5, Nome = "Mochila", Preco = 150.15, Estoque = 50, Categoria = "Acessórios" },
+            new Produto { Id = 6, Nome = "Relógio", Preco = 320.99, Estoque = 15, Categoria = "Acessórios" },
+            new Produto { Id = 7, Nome = "Cadeira", Preco = 260.20, Estoque = 150, Categoria = "Móveis" },
+            new Produto { Id = 8, Nome = "Mesa", Preco = 930, Estoque = 30, Categoria = "Móveis" },
+            new Produto { Id = 9, Nome = "Smart TV", Preco = 1500.90, Estoque = 5, Categoria = "Eletrônicos" },
+            new Produto { Id = 10, Nome = "Notbook", Preco = 6900.75, Estoque = 100, Categoria = "Eletrônicos" }
+        };
+        return produtos;
+    }
+    public static void Mostrar(IEnumerable<Produto> produtos)
+    {
+        foreach (var item in produtos)
+        {
+            Console.WriteLine($"{item.Id} - {item.Nome} - {item.Preco} - {item.Estoque} - {item.Categoria}");            
+        }
+    }
+
+}
